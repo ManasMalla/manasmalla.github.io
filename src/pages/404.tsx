2dd5d5aa3716ac009bcf707848eaab7edb4fbbc6
Manas Malla <manasmalla.dev@gmail.com>
2024-09-22 15:57:28 +0530

    apos issue
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     <div className="flex flex-col items-center">
       <img src="https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif" />
       <div className="-translate-y-[100%] flex flex-col">
-        <p className="text-4xl pb-6">Oops, looks like you're lost!</p>
+        <p className="text-4xl pb-6">Oops, looks like you&apos;re lost!</p>
         <Link
           href={"/"}
           className="px-6 w-full text-center rounded-lg py-3 bg-amber-400"
